17c4e8622a0c5aef0fdf08fc4ba3081960682807
NguyenQuocLong <quoclongdng@gmail.com>
2021-10-20 21:34:09 +0700

    first

---
+++ resources/views/master.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+    @include('partials.head-css')
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  @include('partials.header')
+
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    @include('partials.menu')
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">
+                @yield('title')
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="container-fluid">
+
+        @yield('content')
+
+      </div>
+    </div>
+
+  </div>
+
+  @include('partials.bottom')
+</div>
+</body>
+</html>
---
+++ resources/views/partials/bottom.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <!-- Default to the left -->
+    <strong>Copyright &copy; Quốc Long.</strong> All rights reserved.
+</footer>
